3841bcf958569993ee3c7e12f36b293b3881bd23
Kusal47 <kusalaryal47@gmail.com>
2023-12-31 19:47:01 +0545

    login page created without validation

--- lib/view/pages/auth/login_page.dart
+++ lib/view/pages/auth/login_page.dart
@@ -1,8 +1,7 @@
-
 import 'package:flutter/material.dart';
 
 import '../../../core/routes/routes_name.dart';
-
+import '../../../resources/custom_textfield.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -13,128 +12,75 @@ class LoginPage extends StatelessWidget {
     _userNameController.text = "admin";
     _passwordController.text = "abcd123";
     return Scaffold(
-        backgroundColor: const Color(0xff262e38),
-        body: Row(
+        // backgroundColor: const Color(0xff262e38),
+        body: Center(
+      child: Card(
+        child: Container(
+          color: Colors.white,
+          width: 500,
+          height: 500,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                    
-                      Text(
-                        " Unity Aid Hub",
-                        // textAlign: TextAlign.left,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        "\"Web-based Crowdfunding Platform\"",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w300,
-                            color: Colors.white),
-                      ),
-                     
-                    ],
+                const Text(
+                  "Log Into Unity Aid Hub ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Expanded(
-                    // flex: 2,
-                    child: Container(
-                  color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "Log Into Unity Aid Hub ",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const Text(
-                          "Enter your Login Details Below",
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.w500),
-                        ),
-                        const SizedBox(
-                          height: 50,
-                        ),
-                        const Row(
-                          children: [
-                            Text(" Admin Login"),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: TextFormField(
-                            controller: _userNameController,
-                            decoration: const InputDecoration(
-                              labelText: "Username",
-                              hintText: "Enter Username",
-                              border: OutlineInputBorder(),
-                              prefixIcon: Icon(Icons.person),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        const Row(
-                          children: [
-                            Text(" Password"),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: TextFormField(
-                            controller: _passwordController,
-                            //  adda a prefix icon
-
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                              labelText: "Password",
-                              hintText: "Enter Password",
-                              border: OutlineInputBorder(),
-                              prefixIcon: Icon(Icons.lock),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-
-                        // submit button decorated
-                        ElevatedButton(
-                          onPressed: () {
-                              Navigator.pushNamed(context, RouteName.shell);
-                          
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color.fromARGB(255, 14, 220, 155),
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 50, vertical: 20),
-                          ),
-                          child: const Text(
-                            "Login",
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black),
-                          ),
-                        ),
-                      ],
-                    ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Enter your Login Details Below",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
                   ),
-                ))
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                CustomFields(
+                  controller: _userNameController,
+                  title: "Admin Login",
+                  icon: Icons.person,
+                  hintText: "Enter Username",
+                  labelText: "Username",
+                ),
+                CustomFields(
+                  controller: _passwordController,
+                  isPassword:true,
+                  title: "Password",
+                  icon: Icons.person,
+                  hintText: "Enter Password",
+                  labelText: "Password",
+                ),
+
+                // submit button decorated
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, RouteName.shell);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF44F249),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 20),
+                  ),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
               ],
-            )
-         );
+            ),
+          ),
+        ),
+      ),
+    ));
   }
 }
