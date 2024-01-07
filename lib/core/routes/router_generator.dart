7ac3eacfb95a703c185f2b5fc8093d12833e8c37
Kusal47 <kusalaryal47@gmail.com>
2024-01-07 23:02:23 +0545

    routes and name for pages added

--- lib/core/routes/router_generator.dart
+++ lib/core/routes/router_generator.dart
@@ -1,46 +1,122 @@
 import 'package:flutter/material.dart';
-
 import 'package:unity_admin/core/routes/routes_name.dart';
-
-import '../../view/agent_view/agent_detail.dart';
-import '../../view/balance/balance_page.dart';
-import '../../view/history/history_page.dart';
-import '../../view/order_screen/order_screen.dart';
+import 'package:unity_admin/view/home_screen/home_screen.dart';
+import 'package:unity_admin/view/pages/auth/register_page.dart';
 import '../../view/pages/auth/login_page.dart';
-import '../../view/pages/sidebar_pages/shell_screen.dart';
-import '../../view/result_screen/result_screen.dart';
+import '../../view/pages/view_Pages/add_post.dart';
+import '../../view/pages/view_Pages/ads_management.dart';
+import '../../view/pages/view_Pages/blog_section.dart';
+import '../../view/pages/view_Pages/category.dart';
+import '../../view/pages/view_Pages/comments_crud.dart';
+import '../../view/pages/view_Pages/pages_section.dart';
+import '../../view/pages/view_Pages/post_verification.dart';
+import '../../view/pages/view_Pages/roles_management.dart';
+import '../../view/pages/view_Pages/user_management.dart';
 
-class Routes {
-  static Route<dynamic> generateRoutes(RouteSettings settings) {
-    switch (settings.name) {
+  Widget? generateRoutes(RouteSettings settings) {
+     final uri = Uri.parse(settings.name!);
+    switch (uri.path) {
       case RouteName.login:
-        return MaterialPageRoute(builder: (context) => const LoginPage());
+        return  const LoginPage();
 
-      case RouteName.shell:
-        return MaterialPageRoute(builder: (context) => ShellScreen());
+      case RouteName.dashboard:
+        return const HomeScreen();
 
-      case RouteName.order:
-        return MaterialPageRoute(builder: (context) => OrderScreen());
+      case RouteName.usermanagement:
+        return  const UserManagement();
 
-      case RouteName.history:
-        return MaterialPageRoute(builder: (context) => HistoryPage());
+      case RouteName.rolesmanagement:
+        return  const RolesManagement();
 
-      case RouteName.result:
-        return MaterialPageRoute(builder: (context) => ResultPage());
-      case RouteName.balance:
-        return MaterialPageRoute(builder: (context) => BalancePage());
+      case RouteName.postverify:
+        return  const PostVerification();
+      
+      case RouteName.postadd:
+        return  const AddPost();
 
+      case RouteName.adsmanagement:
+        return  const AdsManagement();
 
-      case RouteName.agent:
-        return MaterialPageRoute(builder: (context) => AgentDetails());
+      case RouteName.category:
+        return  const CategoryPage();
+
+      case RouteName.comments:
+        return  const CommentCRUD();
+
+      case RouteName.pagesection:
+        return  const PagesSection();
+
+      case RouteName.blogs:
+        return  const BlogSection();
+
+      case RouteName.register:
+        return  const RegisterView();
+
       default:
-        return MaterialPageRoute(
-          builder: (BuildContext context) => Scaffold(
-            body: Center(
-              child: Text("No Route Defined for ${settings.name}"),
-            ),
+        return Scaffold(
+          body: Center(
+            child: Text("No Route Defined for ${settings.name}"),
           ),
         );
     }
   }
-}
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// import 'package:unity_admin/core/routes/routes_name.dart';
+// import 'package:unity_admin/view/pages/auth/register_page.dart';
+
+// import '../../view/agent_view/agent_detail.dart';
+// import '../../view/balance/balance_page.dart';
+// import '../../view/history/history_page.dart';
+// import '../../view/order_screen/order_screen.dart';
+// import '../../view/pages/auth/login_page.dart';
+// import '../../view/pages/sidebar_pages/shell_screen.dart';
+
+// class Routes {
+//   static Route<dynamic> generateRoutes(RouteSettings settings) {
+//     switch (settings.name) {
+//       case RouteName.login:
+//         return MaterialPageRoute(builder: (context) => const LoginPage());
+
+//       case RouteName.shell:
+//         return MaterialPageRoute(builder: (context) => ShellScreen());
+
+//       case RouteName.order:
+//         return MaterialPageRoute(builder: (context) => const OrderScreen());
+
+//       case RouteName.history:
+//         return MaterialPageRoute(builder: (context) => const HistoryPage());
+
+//       case RouteName.result:
+//         return MaterialPageRoute(builder: (context) => const RegisterView());
+//       case RouteName.balance:
+//         return MaterialPageRoute(builder: (context) => const BalancePage());
+
+
+//       case RouteName.agent:
+//         return MaterialPageRoute(builder: (context) => const AgentDetails());
+//       default:
+//         return MaterialPageRoute(
+//           builder: (BuildContext context) => Scaffold(
+//             body: Center(
+//               child: Text("No Route Defined for ${settings.name}"),
+//             ),
+//           ),
+//         );
+//     }
+//   }
+// }
+
+
+
+
+
--- lib/view/pages/sidebar_pages/shell_screen.dart
+++ lib/view/pages/sidebar_pages/shell_screen.dart
@@ -1,30 +1,30 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
 
-import '../../../side_menu.dart';
-import '../../../view_model/sidebar_view_model.dart';
+// import '../../../side_menu.dart';
+// import '../../../view_model/sidebar_view_model.dart';
 
 
-class ShellScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final sideBarViewModel = Provider.of<SideBarViewModel>(context);
-    print(sideBarViewModel.currentPage);
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                if (MediaQuery.of(context).size.width > 800) SideMenu(),
-                Expanded(
-                  child: sideBarViewModel.currentPage,
-               ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+// class ShellScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final sideBarViewModel = Provider.of<SideBarViewModel>(context);
+//     print(sideBarViewModel.currentPage);
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: Row(
+//               children: [
+//                 if (MediaQuery.of(context).size.width > 800) SideMenu(),
+//                 Expanded(
+//                   child: sideBarViewModel.currentPage,
+//                ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
