0b2d4d875da62e1d3cbd21c73dfe02842b345a49
Kusal47 <kusalaryal47@gmail.com>
2024-01-10 22:21:21 +0545

    colors added

--- lib/core/theme/app_color.dart
+++ lib/core/theme/app_color.dart
@@ -10,4 +10,6 @@ class AppColor {
   // static Color greyColor = Color(0xFFE0E0E0)
   static Color greyColor = Color(0xFFF4F4F5);
   static Color redColor = Color.fromARGB(255, 255, 0, 0);
+  static Color textColor = Color(0xFF7D7493);
+  static Color borderColor =  Color(0xFFCCCCCC);
 }
