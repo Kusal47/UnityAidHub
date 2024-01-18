f0e487308d82f6903ecfb084aa4dd44ad7ef2355
Kusal47 <kusalaryal47@gmail.com>
2024-01-18 14:16:06 +0545

    multi provider configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'dart:io';
+
+import 'package:desktop_window/desktop_window.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:unity_admin/locator.dart';
 import 'package:unity_admin/view_model/category_view_model.dart';
 import 'core/routes/router_generator.dart';
 import 'core/routes/routes_name.dart';
 import 'core/theme/app_color.dart';
+import 'view_model/add_post_view_model.dart';
 import 'view_model/login_signup_view_model.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
 }
 
@@ -20,25 +26,27 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<LoginViewModel>(
-              create: (_) => LoginViewModel()),
+        ChangeNotifierProvider<LoginViewModel>(create: (_) => LoginViewModel()),
         ChangeNotifierProvider<RegisterViewModel>(
-              create: (_) => RegisterViewModel()),
+            create: (_) => RegisterViewModel()),
         ChangeNotifierProvider<LogOutViewModel>(
-              create: (_) => LogOutViewModel()),
+            create: (_) => LogOutViewModel()),
         ChangeNotifierProvider<CategoryViewModel>(
-              create: (_) => CategoryViewModel()),
+            create: (_) => CategoryViewModel()),
+        ChangeNotifierProvider<AddPostViewModel>(
+            create: (_) => AddPostViewModel()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: ' Unity Aid Hub ',
         darkTheme: ThemeData(
           // brightness: Brightness.dark,
-    
+
           appBarTheme: AppBarTheme(
-              iconTheme: const IconThemeData().copyWith(color: AppColor.darkColor),
+              iconTheme:
+                  const IconThemeData().copyWith(color: AppColor.darkColor),
               // backgroundColor: Colors.white,
-              titleTextStyle:  TextStyle(
+              titleTextStyle: TextStyle(
                 color: AppColor.whiteColor,
                 fontFamily: 'Montserrat',
               )),
@@ -46,28 +54,28 @@ class _MyAppState extends State<MyApp> {
           // backgroundColor: const Color(0xFF121212),
           primaryColor: AppColor.darkColor,
           // // accentColor: const Color(0xFF1DB954),
-          iconTheme: const IconThemeData().copyWith(color:  AppColor.darkColor),
+          iconTheme: const IconThemeData().copyWith(color: AppColor.darkColor),
           fontFamily: 'Montserrat',
-          textTheme:  TextTheme(
+          textTheme: TextTheme(
             headline2: TextStyle(
-              color:  AppColor.darkColor,
+              color: AppColor.darkColor,
               fontSize: 32.0,
               fontWeight: FontWeight.bold,
             ),
             headline4: TextStyle(
               fontSize: 12.0,
-              color:  AppColor.darkColor,
+              color: AppColor.darkColor,
               fontWeight: FontWeight.w500,
               letterSpacing: 2.0,
             ),
             bodyText1: TextStyle(
-              color:  AppColor.darkColor,
+              color: AppColor.darkColor,
               fontSize: 14.0,
               fontWeight: FontWeight.w600,
               letterSpacing: 1.0,
             ),
             bodyText2: TextStyle(
-              color:  AppColor.darkColor,
+              color: AppColor.darkColor,
               letterSpacing: 1.0,
             ),
           ),
