3ac7114dddd583dfbe9055e020a357f5ec907b4e
Kusal47 <kusalaryal47@gmail.com>
2024-01-18 15:24:21 +0545

    controllers

--- lib/view_model/add_post_view_model.dart
+++ lib/view_model/add_post_view_model.dart
@@ -1,17 +1,14 @@
-import 'dart:io';
 import 'dart:typed_data';
 import 'package:logger/logger.dart';
-import 'package:path/path.dart' as path;
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:unity_admin/core/theme/app_color.dart';
 
 class AddPostViewModel extends ChangeNotifier {
-  QuillController controller = QuillController.basic();
+  QuillController quillcontroller = QuillController.basic();
+  TextEditingController fundController = TextEditingController();
   TextEditingController titlecontroller = TextEditingController();
-  TextEditingController titlecontroller1 = TextEditingController();
   Logger logger = Logger();
   bool isChecked = false;
   // bool get _isChecked => isChecked;
@@ -20,40 +17,25 @@ class AddPostViewModel extends ChangeNotifier {
   var selectedColor = AppColor.borderColor;
   // String get _selectedCountry => selectedCountry;
   var selectedCategory;
- List<Uint8List> fileBytesList = [];
- List<Uint8List> get _fileBytesList =>fileBytesList;
-  List<String> fileNamesList = [];
-  List<String> get _fileNamesList =>fileNamesList;
-  Future<void> addpost(BuildContext context) async {
+
+
+  Future<void> submitPost(BuildContext context) async {
     try {} catch (e) {}
 
-    print('Category Added as ${titlecontroller.text}');
-    controller.clear();
+    // print('Category Added as ${titlecontroller.text}');
+    quillcontroller.clear();
     titlecontroller.clear();
-    titlecontroller1.clear();
+    fundController.clear();
   }
 
 
-  Future<void> pickFiles() async {
-    FilePickerResult? result =
-        await FilePicker.platform.pickFiles(allowMultiple: true);
 
-    if (result != null) {
-      List<Uint8List> bytesList =
-          result.files.map((file) => file.bytes!).toList();
-      List<String> namesList = result.files.map((file) => file.name).toList();
 
-      
-        _fileBytesList.addAll(bytesList);
-        _fileNamesList.addAll(namesList);
-     
-    }
-  }
   @override
   void dispose() {
     super.dispose();
-    controller.dispose();
+    quillcontroller.dispose();
     titlecontroller.dispose();
-    titlecontroller1.dispose();
+    fundController.dispose();
   }
 }
